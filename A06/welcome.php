08b52866abc2886c9d79b48d967a79e941f81aa6
Joseph Maliza <josephmaliza55@gmail.com>
2024-11-16 22:47:32 +0800

    update

--- A06/welcome.php
+++ A06/welcome.php
@@ -1,12 +1,12 @@
 <?php
-
 session_start();
 
-
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
+    header('Location: login.php');
     exit();
 }
+
+echo "Welcome, " . $_SESSION['email']; 
 
 $servername = "localhost";
 $username = "root";
